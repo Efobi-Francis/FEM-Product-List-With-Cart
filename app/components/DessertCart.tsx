07e6247c2c8e4e5175802415d96bc5e333e9c272
efobifrancis <francisefobi@gmail.com>
2025-05-12 12:40:29 +0100

    added zustand persist

--- app/components/DessertCart.tsx
+++ app/components/DessertCart.tsx
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 export type CartItem = {
     name: string,
@@ -16,58 +17,63 @@ export type CartState = {
     resetCart: ()=> void
 }
 
-export const useStore = create<CartState>((set)=> ({
-  cart: [],
-
-  addToCart:  (item)=> set((state)=> {
-    const existingItem = state.cart.find((cartItem)=> cartItem.name === item.name)
-    if(existingItem) {
+export const useStore = create<CartState>()(
+  persist(
+    (set)=> ({
+    cart: [],
+  
+    addToCart:  (item)=> set((state)=> {
+      const existingItem = state.cart.find((cartItem)=> cartItem.name === item.name)
+      if(existingItem) {
+        const updatedCart = state.cart.map((cartItem)=> {
+          if(cartItem.name === item.name) {
+            const updatedQuantity = cartItem.quantity + item.quantity
+            return{
+              ...cartItem,
+              quantity: updatedQuantity,
+              total_price: item.price * updatedQuantity 
+            }
+          }
+          return cartItem
+        })
+        return {cart: updatedCart}
+      } else {
+        return {cart: [...state.cart, 
+          {
+            ...item, 
+            name: item.name,
+            quantity: item.quantity,
+            total_price: item.total_price
+          }]}
+      }
+    }),
+  
+    removeFromCart: (item)=> set((state)=> {
       const updatedCart = state.cart.map((cartItem)=> {
         if(cartItem.name === item.name) {
-          const updatedQuantity = cartItem.quantity + item.quantity
+          const updatedQuantity = cartItem.quantity - item.quantity
           return{
             ...cartItem,
             quantity: updatedQuantity,
-            total_price: item.price * updatedQuantity 
+            total_price: item.price * updatedQuantity
           }
         }
         return cartItem
       })
       return {cart: updatedCart}
-    } else {
-      return {cart: [...state.cart, 
-        {
-          ...item, 
-          name: item.name,
-          quantity: item.quantity,
-          total_price: item.total_price
-        }]}
-    }
-  }),
-
-  removeFromCart: (item)=> set((state)=> {
-    const updatedCart = state.cart.map((cartItem)=> {
-      if(cartItem.name === item.name) {
-        const updatedQuantity = cartItem.quantity - item.quantity
-        return{
-          ...cartItem,
-          quantity: updatedQuantity,
-          total_price: item.price * updatedQuantity
-        }
-      }
-      return cartItem
-    })
-    return {cart: updatedCart}
-  }),
-
-  deleteFromCart: (item) => set((state) => {
-    const updatedCart = state.cart.filter((cartItem) => cartItem.name !== item.name)
-    return { cart: updatedCart }
-  }),
-
-  resetCart: () => set(() => {
-    // Clear the cart by setting it to an empty array
-    return { cart: [] }
-  }),
-
-}))
+    }),
+  
+    deleteFromCart: (item) => set((state) => {
+      const updatedCart = state.cart.filter((cartItem) => cartItem.name !== item.name)
+      return { cart: updatedCart }
+    }),
+  
+    resetCart: () => set(() => {
+      // Clear the cart by setting it to an empty array
+      return { cart: [] }
+    }),
+  
+    }),
+    {name: 'dessert-storage'},
+  ),
+)
